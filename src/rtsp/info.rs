fc5e3a2757bd830d993aadc23c155c7c6cb94cd0
r4v3n6101 <raven6107@gmail.com>
2024-02-01 22:56:28 +0300

    Simplify InfoResponse

--- src/rtsp/info.rs
+++ src/rtsp/info.rs
@@ -15,16 +15,16 @@ pub struct InfoRequest {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct InfoResponse {
-    #[serde(rename = "acl")]
-    pub access_control_level: u8,
+    //#[serde(rename = "acl")]
+    //pub access_control_level: u8,
     #[serde(rename = "deviceid")]
     pub device_id: String,
     pub features: String,
     pub flags: String,
-    #[serde(rename = "gcgl")]
-    pub group_containing_discoverable_leader: u8,
-    #[serde(rename = "gid")]
-    pub group_uuid: String,
+    //#[serde(rename = "gcgl")]
+    //pub group_containing_discoverable_leader: u8,
+    //#[serde(rename = "gid")]
+    //pub group_uuid: String,
     pub manufacturer: String,
     pub model: String,
     pub name: String,
@@ -32,34 +32,32 @@ pub struct InfoResponse {
     pub protocol_version: String,
     #[serde(rename = "rsf")]
     pub required_sender_flags: String,
-    #[serde(rename = "serialNumber")]
-    pub serial_number: String,
     #[serde(rename = "srcvers")]
     pub source_version: String,
-    #[serde(rename = "pi")]
-    pub pairing_uuid: String,
-    #[serde(rename = "pk")]
-    pub pubkey: String,
+    // #[serde(rename = "pi")]
+    // pub pairing_uuid: String,
+    // #[serde(rename = "pk")]
+    // pub pubkey: String,
 }
 
 impl Default for InfoResponse {
     fn default() -> Self {
         Self {
-            access_control_level: 0,
+            //access_control_level: 0,
             device_id: MAC_ADDR.into(),
             features: FEATURES.into(),
             flags: "0x4".into(),
-            group_containing_discoverable_leader: 0,
-            group_uuid: GROUP_UUID.into(),
+            //group_containing_discoverable_leader: 0,
+            //group_uuid: GROUP_UUID.into(),
             manufacturer: env!("CARGO_PKG_AUTHORS").into(),
             model: env!("CARGO_PKG_NAME").into(),
             name: env!("CARGO_PKG_NAME").into(),
             protocol_version: "1.1".into(),
             required_sender_flags: "0x0".into(),
-            serial_number: MAC_ADDR.into(),
+            //serial_number: MAC_ADDR.into(),
             source_version: "366.0".into(),
-            pairing_uuid: GROUP_UUID.into(),
-            pubkey: Default::default(),
+            //pairing_uuid: GROUP_UUID.into(),
+            //pubkey: Default::default(),
         }
     }
 }
