6d77db1e87a553291e0c65e9ad35fa4db4f2bc07
r4v3n6101 <raven6107@gmail.com>
2024-07-20 19:27:00 +0300

    nix flake to build package

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description =
+    "A server that process audio and video sent by AirPlay protocol";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+        myRustBuild = rustPlatform.buildRustPackage {
+          pname = manifest.name;
+          version = manifest.version;
+          src = pkgs.lib.cleanSource ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = [ pkgs.libiconv ];
+        };
+      in {
+        defaultPackage = myRustBuild;
+        devShell = pkgs.mkShell {
+          buildInputs =
+            [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
+        };
+      });
+}
