1f365415cd350c7f1f6b6d8ba435a83bedefdfba
r4v3n6101 <raven6107@gmail.com>
2025-02-17 19:59:27 +0300

    jitter buffer: change implementation to mutex'ed variant

--- lib/src/util/jitter.rs
+++ lib/src/util/jitter.rs
@@ -1,18 +1,42 @@
 use std::{
-    collections::BTreeMap,
+    cmp::Ordering,
+    collections::{binary_heap::PeekMut, BinaryHeap},
+    sync::Mutex,
     time::{Duration, Instant},
 };
 
 #[derive(Clone, Copy)]
 struct Entry<T> {
+    idx: u64,
     arrival_time: Instant,
     timestamp: Duration,
     value: T,
 }
 
+impl<T> Ord for Entry<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.idx.cmp(&other.idx).reverse()
+    }
+}
+
+impl<T> PartialOrd for Entry<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.idx.partial_cmp(&other.idx).map(Ordering::reverse)
+    }
+}
+
+impl<T> Eq for Entry<T> {}
+
+impl<T> PartialEq for Entry<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.idx.eq(&other.idx)
+    }
+}
+
 impl<T> Entry<T> {
     fn map<U>(self, f: impl FnOnce(T) -> U) -> Entry<U> {
         Entry {
+            idx: self.idx,
             arrival_time: self.arrival_time,
             timestamp: self.timestamp,
             value: f(self.value),
@@ -20,67 +44,90 @@ impl<T> Entry<T> {
     }
 }
 
-pub struct Buffer<T> {
-    // TODO : use set and hash/eq by number inside of struct
-    entries: BTreeMap<u64, Entry<T>>,
+struct Inner<T> {
+    entries: BinaryHeap<Entry<T>>,
     last_entry: Option<Entry<()>>,
+    last_popped_idx: u64,
     buf_depth: Duration,
+    jitter: Duration,
+}
+
+pub struct Buffer<T> {
+    inner: Mutex<Inner<T>>,
     min_depth: Duration,
     max_depth: Duration,
-    jitter: Duration,
 }
 
 impl<T> Buffer<T> {
     pub fn new(min_depth: Duration, max_depth: Duration) -> Self {
+        assert!(min_depth <= max_depth);
+
         Self {
-            entries: BTreeMap::new(),
-            last_entry: None,
-            buf_depth: min_depth,
+            inner: Mutex::new(Inner {
+                entries: BinaryHeap::new(),
+                last_entry: None,
+                last_popped_idx: 0,
+                buf_depth: min_depth,
+                jitter: Duration::ZERO,
+            }),
+
             min_depth,
             max_depth,
-            jitter: Duration::ZERO,
         }
     }
 
     // TODO : replace Duration with samples mb
-    pub fn insert(&mut self, idx: u64, timestamp: Duration, value: T) {
+    pub fn insert(&self, idx: u64, timestamp: Duration, value: T) {
+        let this = &mut *self.inner.lock().unwrap();
         let entry = Entry {
+            idx,
             arrival_time: Instant::now(),
             timestamp,
             value: (),
         };
-        if let Some(last_entry) = &self.last_entry {
+
+        if let Some(last_entry) = &this.last_entry {
             let delay = entry.arrival_time.duration_since(last_entry.arrival_time);
             let expected_delay = entry.timestamp - last_entry.timestamp;
-            self.jitter += ((delay - expected_delay) - self.jitter) / 16;
-            self.buf_depth = (self.buf_depth + self.jitter).clamp(self.min_depth, self.max_depth);
+            this.jitter += (delay - expected_delay - this.jitter) / 16;
+            this.buf_depth = (this.buf_depth + this.jitter).clamp(self.min_depth, self.max_depth);
         }
-        self.last_entry = Some(entry);
+        this.last_entry = Some(entry);
 
-        // TODO : GC too old packets
-        self.entries.insert(idx, entry.map(|_| value));
+        // Discard late packets
+        if idx <= this.last_popped_idx {
+            return;
+        }
+
+        this.entries.push(entry.map(|_| value));
     }
 
-    pub fn pop(&mut self) -> Output<T> {
-        let Some((_, pkt)) = self.entries.pop_first() else {
-            return Output::EmptyBuffer;
-        };
+    pub fn pop(&self) -> Output<T> {
+        let this = &mut *self.inner.lock().unwrap();
 
         let now = Instant::now();
-        let pkt_ready = pkt.arrival_time + self.buf_depth;
-        match pkt_ready.checked_duration_since(now) {
-            Some(wait_time) => Output::NotYet { wait_time },
-            None => match (pkt_ready + self.buf_depth).checked_duration_since(now) {
-                Some(latency) => Output::TooLate { latency },
-                None => Output::Value(pkt.value),
-            },
+        let mut data = Vec::new();
+        while let Some(entry) = this.entries.peek_mut() {
+            let pkt_ready = entry.arrival_time + this.buf_depth;
+            match pkt_ready.checked_duration_since(now) {
+                Some(wait_time) => {
+                    return Output { wait_time, data };
+                }
+                None => {
+                    let entry = PeekMut::pop(entry);
+                    data.push(entry.value);
+                }
+            }
+        }
+
+        Output {
+            wait_time: this.buf_depth,
+            data,
         }
     }
 }
 
-pub enum Output<T> {
-    EmptyBuffer,
-    NotYet { wait_time: Duration },
-    TooLate { latency: Duration },
-    Value(T),
+pub struct Output<T> {
+    pub wait_time: Duration,
+    pub data: Vec<T>,
 }
